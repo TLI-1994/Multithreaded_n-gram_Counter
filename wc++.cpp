37e656d3ffd381f55c9e78650fc8b0170b2b383a
TLI-1994 <cutianjiao@gmail.com>
2021-12-03 01:31:40 -0500

    Format input for the executable

--- wc++.cpp
+++ wc++.cpp
@@ -2,13 +2,21 @@
 
 #include "word_counter.hpp"
 
-// this program computes word frequencies for all .h and .c files in the given directory and its subdirectories
+/* this program computes n-gram frequencies for all .txt files in the given directory and its subdirectories
+Author: Tianjiao Li @ Cornell MAE
+Inspired by: Sagar Jha's wc++ program
+Highlight: this program is built around the map-reduce pattern in concurrent programming. */
 int main(int argc, char* argv[]) {
     if (argc < 4) {
-        std::cout << "Usage: " << argv[0] << " <dir> <n> <num-threads>" << std::endl;
+        std::cout << "Usage: " << argv[0] << " <dir> -n=<#gram> -t=<#threads>" << std::endl;
         return 1;
     }
-
-    wc::wordCounter word_counter(argv[1], std::stoi(argv[2]), std::stoi(argv[3]));
+    if (std::string(argv[2]).substr(0, 2) != "-n" || std::string(argv[3]).substr(0, 2) != "-t") {
+        std::cout << "Usage: " << argv[0] << " <dir> -n=<#gram> -t=<#threads>" << std::endl;
+        return 1;
+    }
+    uint32_t n = std::stoi(std::string(argv[2]).substr(3));
+    uint32_t t = std::stoi(std::string(argv[3]).substr(3));
+    wc::wordCounter word_counter(argv[1], n, t);
     word_counter.process();
 }
