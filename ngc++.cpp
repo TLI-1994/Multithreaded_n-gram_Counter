464c9ff619b0c29c7d8a03ee4109749969015e56
TLI-1994 <cutianjiao@gmail.com>
2022-03-01 02:32:36 -0500

    Update argument order

--- ngc++.cpp
+++ ngc++.cpp
@@ -7,12 +7,12 @@ Author: Tianjiao Li @ Cornell MAE
 Inspired by: Sagar Jha's wc++ program
 Highlight: this program is built around the map-reduce pattern in concurrent programming. */
 int main(int argc, char* argv[]) {
-    if (argc < 4 || std::string(argv[2]).substr(0, 3) != "-n=" || std::string(argv[3]).substr(0, 3) != "-t=") {
-        std::cout << "Usage: " << argv[0] << " <dir> -n=<#gram> -t=<#threads>" << std::endl;
+    if (argc < 4 || std::string(argv[1]).substr(0, 3) != "-n=" || std::string(argv[2]).substr(0, 3) != "-t=") {
+        std::cout << "Usage: " << argv[0] << " -n=<#gram> -t=<#threads> <dir>" << std::endl;
         return 1;
     }
-    uint32_t n = std::stoi(std::string(argv[2]).substr(3));
-    uint32_t t = std::stoi(std::string(argv[3]).substr(3));
-    wc::wordCounter word_counter(argv[1], n, t);
+    uint32_t n = std::stoi(std::string(argv[1]).substr(3));
+    uint32_t t = std::stoi(std::string(argv[2]).substr(3));
+    wc::wordCounter word_counter(argv[3], n, t);
     word_counter.process();
 }
